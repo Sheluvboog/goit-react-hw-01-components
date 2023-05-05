3d42e15b19f50641bfa48a5777bd5ff05e7990dc
Sheluvboog <111418674+Sheluvboog@users.noreply.github.com>
2023-05-05 17:52:50 +0300

    Додає ДЗ 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import { Profile } from "./profile/Profile";
-import user from "./profile/user.json";
+import user from './profile/user.json' // дані
 import { Statistics } from "./statistics/Statistics";
-import data from "./statistics/data.json";
+import data from './statistics/data.json' // дані
 import { FriendList } from "./friendList/FriendList";
-import friends from "./friendList/friends.json"
-import { TransactionHistory } from "./transactions/TransactionHistory";
-import transactions from "./transactions/transactions.json"
+import friends from './friendList/friends.json' // дані
+import { TransactionHistory } from "./transactionHistory/TransactionHistory";
+import transactions from './transactionHistory/transactions.json' // дані
 
-
-
-
+// Експортуємо наш компонент App.
+// Це означає, що ми можемо використовувати цей компонент в інших частинах нашої програми.
 export const App = () => {
   return (
+    // створення div-контейнера для компонентів
     <div
       style={{
         display: 'flex',
@@ -22,10 +22,24 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile items={user} />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions}/>
+      <Profile
+      // Відображення інформації про користувача з файлу .json
+      // Передаємо властивості (props) до компонента.
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      {/* Відображення статистики завантажень з data.json */}
+      <Statistics title="Upload stats" stats={data}/>
+
+      {/* Відображення списку друзів з friends.json */}
+      <FriendList friends={friends}/>
+
+      {/* Відображення історії транзакцій з transactions.json */}
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,34 +1,45 @@
-import PropTypes from 'prop-types';
-import css from "./FriendList.module.css";
-import { FriendListItem } from "./FriendListItem";
+import { FriendListItem } from './FriendListItem'; // елемент списку
+import PropTypes from 'prop-types'; // типи пропсів
+import css from './FriendList.module.css'; // стилізація компонента
 
-
+// компонент який приймає список друзів в якості пропсів
 export const FriendList = ({ friends }) => {
-    return (
-        <ul className={css.friendlist}>
+  return (
+    <ul className={css.friendList}>
 
-            {friends.map(friend => {
-                return (
-                    <FriendListItem
-                        key={friend.id}
-                        avatar={friend.avatar}
-                        name={friend.name}
-                        isOnline={friend.isOnline}
-                    />
-                );
-            })}
-        </ul>
-    );
+      {/* Перебір друзів */}
+      {friends.map(friend => {
+        return (
+
+          // повернення 1 карточки друга
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        
-    PropTypes.shape({
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired,
-        id: PropTypes.number.isRequired,
-    }).isRequired
-    ).isRequired
-}
+
+  // Перевірка, чи переданий параметр є масивом,
+  // і приймає PropTypes.shape в якості параметра.
+  friends: PropTypes.arrayOf(
+
+  // shape - визначає форму об'єкта, який передається у властивість
+  // і вимагає, щоб властивості цього об'єкта відповідали певній формі (типу даних)
+
+  PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired
+  ).isRequired,
+};
+
+// Діма Берестень
