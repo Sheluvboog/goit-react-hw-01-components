5f21db7729c748a533c5a4ebc1fa394b2f9c020f
Sheluvboog <111418674+Sheluvboog@users.noreply.github.com>
2023-05-05 17:54:13 +0300

    Додає ДЗ 1

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,51 +1,43 @@
-import PropTypes from 'prop-types'; // типи пропсів
-import css from './Profile.module.css'; // стилізація компонента
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
-// компонент профілю користувача з переданих властивостей
-export const Profile = ({ userName, tag, location, avatar, stats }) => {
-  return (
-    // блок з інформацією про користувача та статистикою
+export const Profile = ({ items: { avatar, username, tag, location, stats } }) => {
+    return (
     <div className={css.profile}>
-
-      {/* інформація про користувача */}
-      <div className={css.description}>
-        <img src={avatar} alt={userName} className={css.avatar} />
-        <p className={css.name}>{userName}</p>
-        <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
-
-      {/* статистика користувача */}
-
-      <ul className={css.stats}>
-        <li className={css.item}>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantiny}>{stats.followers}</span>
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Views</span>
-          <span className={css.quantiny}>{stats.views}</span>
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantiny}>{stats.likes}</span>
-        </li>
-      </ul>
+        <div className={css.description}>
+            <img src={avatar} url={username} className={css.avatar} />
+            <p className={css.name}>{username}</p>
+            <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>    
+        </div>
+            <ul className={css.stats}>
+                <li className={css.item_stats}>
+                    <span className={css.label}>Followers</span>
+                    <span className={css.quantity}>{stats.followers}</span>
+                </li>
+                <li className={css.item_stats}>
+                    <span className={css.label}>Views</span>
+                    <span className={css.quantity}>{stats.views}</span>
+                </li>
+                <li className={css.item_stats}>
+                    <span className={css.label}>Likes</span>
+                    <span className={css.quantity}>{stats.likes}</span>
+                </li>
+            </ul>
     </div>
-  );
-};
+    )
+}
 
-// Типізація для компонента
 Profile.propTypes = {
-  userName: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
-// Діма Берестень
+    items: PropTypes.shape({
+        avatar: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        tag: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        stats: PropTypes.shape({
+            followers: PropTypes.number.isRequired,
+            likes: PropTypes.number.isRequired,
+            views: PropTypes.number.isRequired,
+        }).isRequired,
+        }).isRequired,
+    }
